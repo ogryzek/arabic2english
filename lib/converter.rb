4391ea6f68e77728ed24448757a1158ab3d61994
ogryzek <drewo@activestate.com>
2015-06-13 10:04:31 -0700

    Add method for handing double digits

--- lib/converter.rb
+++ lib/converter.rb
@@ -3,11 +3,23 @@ ONES = [
   'nine', 'ten'
 ]
 
+TEENS = [
+  'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen',
+  'seventeen', 'eighteen', 'nineteen'
+]
+
 def converter(num)
-  case num.to_s.length
+  num_str = num.to_s
+
+  case num_str.length
   when 1
     ONES[num.to_i]
   when 2
-    "ten"
+    num_arr = num_str.split("")
+    convert_double_digits(num_arr)
   end
+end
+
+def convert_double_digits(num)
+  "ten"
 end
